7e7b0af36d8cc4d5923831c38f000f8d3f9afd8b
chanyikchong <1642508849@qq.com>
2022-08-03 19:25:55 +0100

    PEP8 style

--- scheduler/BaGTI/src/utils.py
+++ scheduler/BaGTI/src/utils.py
@@ -23,7 +23,7 @@ def reduce(l):
     low, high = [], []
     for i in range(0, len(l)):
         res.append(statistics.mean(l[max(0, i - n):min(len(l), i + n)]))
-        low.append(min(l[max(0, i - n):min(len(l), i + n)]));
+        low.append(min(l[max(0, i - n):min(len(l), i + n)]))
         high.append(max(l[max(0, i - n):min(len(l), i + n)]))
     return res, low, high
 
@@ -50,7 +50,8 @@ def load_energy_data():
         for j in range(50):
             cpu.append(data[i][j] / 100)
             oneHot = [0] * 50
-            if int(data[i][j + 50]) >= 0: oneHot[int(data[i][j + 50])] = 1
+            if int(data[i][j + 50]) >= 0:
+                oneHot[int(data[i][j + 50])] = 1
             alloc.append(oneHot)
         cpu = np.array([cpu]).transpose()
         alloc = np.array(alloc)
@@ -59,25 +60,26 @@ def load_energy_data():
     return dataset, len(dataset)
 
 
-def load_energy_latency_data(HOSTS):
-    dataset_path = 'datasets/energy_latency_' + str(HOSTS) + '_scheduling.csv'
+def load_energy_latency_data(hosts):
+    dataset_path = 'datasets/energy_latency_' + str(hosts) + '_scheduling.csv'
     data = pd.read_csv(dataset_path) if os.path.exists(dataset_path) else pd.read_csv('scheduler/BaGTI/' + dataset_path)
     data = data.values.astype(np.float)
-    max_ips_container = max(data.max(0)[HOSTS:2 * HOSTS])
+    max_ips_container = max(data.max(0)[hosts:2 * hosts])
     dataset = []
     print("Dataset size", data.shape[0])
     for i in range(data.shape[0]):
-        cpuH, cpuC, alloc = [], [], []
-        for j in range(HOSTS):
-            cpuH.append(data[i][j] / 100)
-            cpuC.append(data[i][j + HOSTS] / max_ips_container)
-            oneHot = [0] * HOSTS
-            if int(data[i][j + (2 * HOSTS)]) >= 0: oneHot[int(data[i][j + (2 * HOSTS)])] = 1
-            alloc.append(oneHot)
-        cpuH = np.array([cpuH]).transpose();
-        cpuC = np.array([cpuC]).transpose()
+        cpu_host, cpu_container, alloc = [], [], []
+        for j in range(hosts):
+            cpu_host.append(data[i][j] / 100)
+            cpu_container.append(data[i][j + hosts] / max_ips_container)
+            one_hot = [0] * hosts
+            if int(data[i][j + (2 * hosts)]) >= 0:
+                one_hot[int(data[i][j + (2 * hosts)])] = 1
+            alloc.append(one_hot)
+        cpu_host = np.array([cpu_host]).transpose()
+        cpu_container = np.array([cpu_container]).transpose()
         alloc = np.array(alloc)
-        dataset.append(((np.concatenate((cpuH, cpuC, alloc), axis=1)), torch.Tensor(
+        dataset.append(((np.concatenate((cpu_host, cpu_container, alloc), axis=1)), torch.Tensor(
             [(data[i][-2] - data.min(0)[-2]) / (data.max(0)[-2] - data.min(0)[-2]),
              max(0, data[i][-1]) / data.max(0)[-1]])))
         if dataset[-1][1][1] > 1:
@@ -86,66 +88,67 @@ def load_energy_latency_data(HOSTS):
     return dataset, len(dataset), max_ips_container
 
 
-def load_energy_latency2_data(HOSTS):
-    dataset_path = 'datasets/energy_latency2_' + str(HOSTS) + '_scheduling.csv'
+def load_energy_latency2_data(hosts):
+    dataset_path = 'datasets/energy_latency2_' + str(hosts) + '_scheduling.csv'
     data = pd.read_csv(dataset_path, header=None) if os.path.exists(dataset_path) else pd.read_csv(
         'scheduler/BaGTI/' + dataset_path, header=None)
     data = data.values.astype(np.float)
-    max_ips_container = max(data.max(0)[HOSTS:2 * HOSTS])
-    max_energy = data.max(0)[3 * HOSTS]
-    max_response = data.max(0)[3 * HOSTS + 1]
+    max_ips_container = max(data.max(0)[hosts:2 * hosts])
+    max_energy = data.max(0)[3 * hosts]
+    max_response = data.max(0)[3 * hosts + 1]
     dataset = []
     print("Dataset size", data.shape[0])
     for i in range(data.shape[0]):
-        cpuH, cpuC, alloc = [], [], []
-        for j in range(HOSTS):
-            cpuH.append(data[i][j] / 100)
-            cpuC.append(data[i][j + HOSTS] / max_ips_container)
-            oneHot = [0] * HOSTS
-            if int(data[i][j + (2 * HOSTS)]) >= 0: oneHot[int(data[i][j + (2 * HOSTS)])] = 1
+        cpu_host, cpu_container, alloc = [], [], []
+        for j in range(hosts):
+            cpu_host.append(data[i][j] / 100)
+            cpu_container.append(data[i][j + hosts] / max_ips_container)
+            oneHot = [0] * hosts
+            if int(data[i][j + (2 * hosts)]) >= 0:
+                oneHot[int(data[i][j + (2 * hosts)])] = 1
             alloc.append(oneHot)
-        cpuH = np.array([cpuH]).transpose();
-        cpuC = np.array([cpuC]).transpose()
+        cpu_host = np.array([cpu_host]).transpose()
+        cpu_container = np.array([cpu_container]).transpose()
         alloc = np.array(alloc)
-        pred_vals = np.broadcast_to(np.array([data[i][3 * HOSTS] / max_energy, data[i][3 * HOSTS + 1] / max_response]),
-                                    (HOSTS, 2))
-        dataset.append(((np.concatenate((cpuH, cpuC, alloc, pred_vals), axis=1)), torch.Tensor(
+        pred_vals = np.broadcast_to(np.array([data[i][3 * hosts] / max_energy, data[i][3 * hosts + 1] / max_response]),
+                                    (hosts, 2))
+        dataset.append(((np.concatenate((cpu_host, cpu_container, alloc, pred_vals), axis=1)), torch.Tensor(
             [(data[i][-2] - data.min(0)[-2]) / (data.max(0)[-2] - data.min(0)[-2]),
              max(0, data[i][-1]) / data.max(0)[-1]])))
     # Normalization by (x - min)/(max - min)
     return dataset, len(dataset), (max_ips_container, max_energy, max_response)
 
 
-def load_stochastic_energy_latency_data(HOSTS):
-    return load_energy_latency_data(HOSTS)
+def load_stochastic_energy_latency_data(hosts):
+    return load_energy_latency_data(hosts)
 
 
-def load_stochastic_energy_latency2_data(HOSTS):
-    return load_energy_latency2_data(HOSTS)
+def load_stochastic_energy_latency2_data(hosts):
+    return load_energy_latency2_data(hosts)
 
 
 def plot_accuracies(accuracy_list, data_type):
-    trainAcc = [i[0] for i in accuracy_list]
-    testAcc = [i[1] for i in accuracy_list]
+    train_acc = [i[0] for i in accuracy_list]
+    test_acc = [i[1] for i in accuracy_list]
     plt.xlabel('Epochs')
     plt.ylabel('Average Training Loss')
-    plt.plot(range(len(trainAcc)), trainAcc, label='Average Training Loss', linewidth=1, linestyle='-', marker='.')
+    plt.plot(range(len(train_acc)), train_acc, label='Average Training Loss', linewidth=1, linestyle='-', marker='.')
     plt.legend(loc=1)
     plt.savefig('graphs/' + data_type + '/training-graph.pdf')
     plt.clf()
     plt.xlabel('Epochs')
     plt.ylabel('Average Testing Loss')
-    plt.errorbar(range(len(testAcc)), testAcc, label='Average Testing Loss', alpha=0.7, \
+    plt.errorbar(range(len(test_acc)), test_acc, label='Average Testing Loss', alpha=0.7,
                  linewidth=1, linestyle='dotted', marker='+')
     plt.legend(loc=4)
     plt.savefig('graphs/' + data_type + '/testing-graph.pdf')
     plt.clf()
     plt.xlabel('Epochs')
     plt.ylabel('Testing Loss')
-    a, b, c = reduce(trainAcc)
-    b2, _, _ = reduce(b);
+    a, b, c = reduce(train_acc)
+    b2, _, _ = reduce(b)
     c2, _, _ = reduce(c)
-    plt.fill_between(np.arange(len(trainAcc)), b2, c2, color='lightgreen', alpha=.5)
-    plt.plot(a, label='Testing Loss', alpha=0.7, color='g', \
+    plt.fill_between(np.arange(len(train_acc)), b2, c2, color='lightgreen', alpha=.5)
+    plt.plot(a, label='Testing Loss', alpha=0.7, color='g',
              linewidth=1, linestyle='-')
     plt.savefig('graphs/' + data_type + '/reduced.pdf')
