8b1847f09c15d3603b09dc975f3cf38c06068a77
chanyikchong <1642508849@qq.com>
2022-08-04 17:07:31 +0100

    convert to package

---
+++ metrics/powermodels/__init__.py
@@ -0,0 +1,7 @@
+from .PM import PM
+from .PMB2s import PMB2s
+from .PMB4ms import PMB4ms
+from .PMB8ms import PMB8ms
+from .PMXeon_X5570 import PMXeon_X5570
+from .PMConstant import PMConstant
+from .PMRaspberryPi import PMRaspberryPi
